d303d9ee451d3dfad612168ef4cbcf111f61bd54
justvietnh <vietnh95@gmail.com>
2022-04-10 15:17:25 +0200

    Add rainbow mode

--- external.js
+++ external.js
@@ -1,5 +1,7 @@
 
 const container = document.querySelector(".sketchWindow")
+const checkRain = document.getElementById('switchRain');
+const checkGrad = document.getElementById('switchGrad');
 
 function createGrid(gridSize){
   for (let rows = 0; rows < gridSize; rows++) {
@@ -15,20 +17,49 @@ function createGrid(gridSize){
     collection[i].style.width = (480/gridSize)+"px";
   }
 }
-
-function recolor(e) {
-  e.style.backgroundColor = "black";
+function checkBox() {
+  const allSwitch = document.getElementsByClassName("switch");
+  for (let box = 0; box < allSwitch.length; box++) {
+    let singleSwitch = allSwitch[box];
+    singleSwitch.addEventListener('click', function(e){
+      console.log(e)
+      
+    })
+  }
 }
+function checkColor(cell){
+  console.log("CHANGE")
+  
+  if (checkRain.checked) {
+    checkGrad.checked = false;
+    return '#'+(0x1000000+Math.random()*0xffffff).toString(16).slice(1,7)
+  } else if(checkGrad.checked) {
+    checkRain.checked = false;
+    console.log(cell.style.currentStyle)
+    return "white"
+  } else {
+    return "black"
+  }
+}
+
+function recolor(cell, color) {
+  let backColor = color;
+  cell.style.backgroundColor = backColor;
+}
+
 function makeHover() {
   const allCells = document.getElementsByClassName("cell");
   for (let cell = 0; cell < allCells.length; cell++) {
     let singleCell = allCells[cell];
     singleCell.addEventListener('mouseover', function(e) {
-    recolor(singleCell);
+      let color = checkColor(cell);
+      recolor(singleCell, color);
     });
   }
 }
+
 createGrid(16);
+checkBox();
 makeHover();
 
 
